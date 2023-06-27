a91364b7e08866259296810743803341a2097612
Praharsh Jain <praharshjain@users.noreply.github.com>
2023-06-27 12:21:41 +0530

    feat: Added object mapper implementation and basic tests (#7)
    Because
    
    - we need a way to convert data from one struct/format to another, and
    the need to do this will only increase with future integrations
    
    This commit
    
    - Adds object mapper implementation to map data from one struct to
    another

--- go.mod
+++ go.mod
@@ -6,13 +6,16 @@ require (
 	github.com/gofrs/uuid v4.4.0+incompatible
 	github.com/instill-ai/protogen-go v0.3.3-alpha.0.20230626081036-adbc33794c42
 	github.com/santhosh-tekuri/jsonschema/v5 v5.3.0
+	github.com/stretchr/testify v1.8.0
 	go.uber.org/zap v1.24.0
 	google.golang.org/protobuf v1.30.0
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.7.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/net v0.7.0 // indirect
@@ -20,4 +23,5 @@ require (
 	golang.org/x/text v0.7.0 // indirect
 	google.golang.org/genproto v0.0.0-20220107163113-42d7afdf6368 // indirect
 	google.golang.org/grpc v1.43.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ pkg/objectmapper/mapper_test.go
@@ -0,0 +1,119 @@
+package objectmapper
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type Nested struct {
+	A *string       `json:"a"`
+	B float32       `json:"b"`
+	C MoreNesting   `json:"c"`
+	F []MoreNesting `json:"f"`
+}
+
+type MoreNesting struct {
+	D []int  `json:"d"`
+	E string `json:"e"`
+}
+
+type StructSrc struct {
+	N Nested `json:"n"`
+}
+
+type Dest struct {
+	A string                 `json:"a" om:"n.a"`
+	B int                    `json:"b" om:"n.b,32"`
+	D *string                `json:"d" om:"n.c.d[1]"`
+	E int                    `json:"e" om:"n.c.e"`
+	F []int                  `json:"f" om:"n.c.d[:]"`
+	G MoreNesting            `json:"g" om:"n.f[0]"`
+	H map[string]interface{} `json:"h" om:"n.f[1]"`
+	I string                 `json:"i" om:"n.f[0].e"`
+	J uint                   `json:"j" om:"n.f[0].d[1]"`
+	K bool                   `json:"k"`
+}
+
+type Dest2 struct {
+	A string                 `json:"a" customTag:"n.a"`
+	B int                    `json:"b" customTag:"n.b;32"`
+	D *string                `json:"d" customTag:"n.c.d[invalid];pj"`
+	E int                    `json:"e" customTag:"n.c.e"`
+	F []int                  `json:"f" customTag:"n.c.d[1:]"`
+	G MoreNesting            `json:"g" customTag:"n.f[100]"`
+	H map[string]interface{} `json:"h" customTag:"n.f[1]"`
+	I string                 `json:"i" customTag:"n.f[-1].e"`
+	J float32                `json:"j" customTag:"n.f[0].d[1]"`
+	K []bool                 `json:"k" customTag:";true"`
+}
+
+func TestTemp(t *testing.T) {
+	str := "abc"
+	str2 := "32"
+	str3 := "pj"
+
+	s := StructSrc{
+		N: Nested{
+			A: &str,
+			B: 123.2,
+			C: MoreNesting{D: []int{23, 32}},
+			F: []MoreNesting{{D: []int{2, 3}, E: "xyz"}, {D: []int{4, 5}, E: "pqr"}},
+		},
+	}
+
+	tests := []struct {
+		name        string
+		source      interface{}
+		expectedErr error
+		exec        func()
+	}{
+		{
+			name:   "master test case 1",
+			source: s,
+			exec: func() {
+				om := ObjectMapper{}
+				d := Dest{}
+				expectedRes := Dest{
+					A: "abc",
+					B: 32,
+					D: &str2,
+					F: []int{23, 32},
+					G: MoreNesting{D: []int{2, 3}, E: "xyz"},
+					H: map[string]any{"d": []any{float64(4), float64(5)}, "e": "pqr"},
+					I: "xyz",
+					J: 3,
+				}
+				assert.Nil(t, om.Map(s, &d))
+				assert.Equal(t, expectedRes, d)
+			},
+		},
+		{
+			name:   "master test case 2",
+			source: s,
+			exec: func() {
+				om := ObjectMapper{Tag: "customTag", DefaultValueSeparator: ";"}
+				d := Dest2{}
+				expectedRes := Dest2{
+					A: "abc",
+					B: 32,
+					D: &str3,
+					F: []int{32},
+					G: MoreNesting{D: []int{4, 5}, E: "pqr"},
+					H: map[string]any{"d": []any{float64(4), float64(5)}, "e": "pqr"},
+					I: "xyz",
+					J: 3,
+					K: []bool{true},
+				}
+				assert.Nil(t, om.Map(s, &d))
+				assert.Equal(t, expectedRes, d)
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			test.exec()
+		})
+	}
+}
