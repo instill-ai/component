e86cdd0f7b0abe7bdb1f68393d7f284b7748f4f7
chuang8511 <a0961192312@gmail.com>
2024-05-10 18:42:51 +0100

    fix: change to header name

--- pkg/connector/pinecone/v0/main.go
+++ pkg/connector/pinecone/v0/main.go
@@ -62,7 +62,7 @@ func newClient(config *structpb.Struct, logger *zap.Logger) *httpclient.Client {
 	)
 
 	c.SetHeader("Api-Key", getAPIKey(config))
-	c.SetHeader("Source-Tag", "instillai")
+	c.SetHeader("User-Agent", "source_tag=instillai")
 
 	return c
 }
