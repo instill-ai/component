a4bbd0c889e7f1e14dc08c2909517103c21fd44d
chuang8511 <a0961192312@gmail.com>
2024-05-24 21:44:26 +0100

    fix: Revert test code

--- operator/text/v0/split_test.go
+++ operator/text/v0/split_test.go
@@ -1,28 +1,28 @@
 package text
 
-// import (
-// 	"context"
-// 	"testing"
+import (
+	"context"
+	"testing"
 
-// 	"google.golang.org/protobuf/types/known/structpb"
-// )
+	"google.golang.org/protobuf/types/known/structpb"
+)
 
-// // TestSplitByToken tests the split by token task
-// func TestSplitByToken(t *testing.T) {
-// 	input := &structpb.Struct{
-// 		Fields: map[string]*structpb.Value{
-// 			"text":  {Kind: &structpb.Value_StringValue{StringValue: "Hello world. This is a test."}},
-// 			"model": {Kind: &structpb.Value_StringValue{StringValue: "gpt-3.5-turbo"}},
-// 		},
-// 	}
-// 	inputs := []*structpb.Struct{
-// 		input,
-// 	}
+// TestSplitByToken tests the split by token task
+func TestSplitByToken(t *testing.T) {
+	input := &structpb.Struct{
+		Fields: map[string]*structpb.Value{
+			"text":  {Kind: &structpb.Value_StringValue{StringValue: "Hello world. This is a test."}},
+			"model": {Kind: &structpb.Value_StringValue{StringValue: "gpt-3.5-turbo"}},
+		},
+	}
+	inputs := []*structpb.Struct{
+		input,
+	}
 
-// 	e := &execution{}
-// 	e.Task = "TASK_SPLIT_BY_TOKEN"
+	e := &execution{}
+	e.Task = "TASK_SPLIT_BY_TOKEN"
 
-// 	if _, err := e.Execute(context.Background(), inputs); err != nil {
-// 		t.Fatalf("splitByToken returned an error: %v", err)
-// 	}
-// }
+	if _, err := e.Execute(context.Background(), inputs); err != nil {
+		t.Fatalf("splitByToken returned an error: %v", err)
+	}
+}
