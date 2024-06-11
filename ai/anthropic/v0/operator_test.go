6e98723c28c6468d856d9e8c187247107c9c599b
andre.liang <54308739+namwoam@users.noreply.github.com>
2024-06-11 14:29:01 +0100

    remove: remove deprecated test

--- ai/anthropic/v0/operator_test.go
+++ ai/anthropic/v0/operator_test.go
@@ -1,37 +1,15 @@
 package anthropic
 
 import (
-	"context"
 	"testing"
 
 	qt "github.com/frankban/quicktest"
 	"github.com/instill-ai/component/base"
 	"go.uber.org/zap"
-	"google.golang.org/protobuf/types/known/structpb"
 )
 
 func TestOperator_Execute(t *testing.T) {
-	c := qt.New(t)
-	ctx := context.Background()
 
-	bc := base.Component{Logger: zap.NewNop()}
-	component := Init(bc)
-
-	c.Run("ok - greet", func(c *qt.C) {
-		exec, err := component.CreateExecution(nil, nil, taskGreet)
-		c.Assert(err, qt.IsNil)
-
-		pbIn, err := structpb.NewStruct(map[string]any{"target": "bolero-wombat"})
-		c.Assert(err, qt.IsNil)
-
-		got, err := exec.Execution.Execute(ctx, []*structpb.Struct{pbIn})
-		c.Check(err, qt.IsNil)
-		c.Assert(got, qt.HasLen, 1)
-
-		// Check JSON in the output string.
-		greeting := got[0].Fields["greeting"].GetStringValue()
-		c.Check(greeting, qt.Equals, "Hello, bolero-wombat!")
-	})
 }
 
 func TestOperator_CreateExecution(t *testing.T) {
