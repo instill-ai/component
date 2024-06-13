6ec3ef9178c4681a1972354ec8e3e69a8486c534
chuang8511 <a0961192312@gmail.com>
2024-06-13 17:46:06 +0100

    feat: dynamically make kebab-case

--- base/formats.go
+++ base/formats.go
@@ -2,7 +2,9 @@ package base
 
 import (
 	"encoding/base64"
+	"regexp"
 	"strings"
+	"unicode"
 
 	"github.com/gabriel-vasile/mimetype"
 	"github.com/santhosh-tekuri/jsonschema/v5"
@@ -196,4 +198,40 @@ func CompileInstillFormat(sch *structpb.Struct) error {
 func TrimBase64Mime(b64 string) string {
 	splitB64 := strings.Split(b64, ",")
 	return splitB64[len(splitB64)-1]
+}
+
+type InstillDynamicFormatTransformer struct{}
+
+func (InstillDynamicFormatTransformer) ConvertToKebab(str string) string {
+	if strings.Index(str, "_") >= 0 {
+		re := regexp.MustCompile(`_`)
+		return strings.ToLower(re.ReplaceAllString(str, "-"))
+	} else if containsCapital(str) {
+		return camelToKebab(str)
+	}
+	return str
+}
+
+func containsCapital(s string) bool {
+	for _, r := range s {
+		if unicode.IsUpper(r) {
+			return true
+		}
+	}
+	return false
+}
+
+func camelToKebab(s string) string {
+	var result strings.Builder
+	for i, r := range s {
+		if unicode.IsUpper(r) {
+			if i != 0 {
+				result.WriteRune('-')
+			}
+			result.WriteRune(unicode.ToLower(r))
+		} else {
+			result.WriteRune(r)
+		}
+	}
+	return result.String()
 }
--- data/bigquery/v0/main.go
+++ data/bigquery/v0/main.go
@@ -252,11 +252,13 @@ func constructTableColumns(myDataset *bigquery.Dataset, ctx context.Context, com
 
 func constructTableProperties(tables []TableColumns) ([]*structpb.Struct, error) {
 	tableProperties := make([]*structpb.Struct, len(tables))
+	transformer := base.InstillDynamicFormatTransformer{}
 
 	for idx, table := range tables {
 		propertiesMap := make(map[string]interface{})
 		for idx, column := range table.Columns {
-			propertiesMap[column.Name] = map[string]interface{}{
+			kebabCaseColumnName := transformer.ConvertToKebab(column.Name)
+			propertiesMap[kebabCaseColumnName] = map[string]interface{}{
 				"title":                column.Name,
 				"instillUIOrder":       idx,
 				"description":          "Column " + column.Name + " of table " + table.TableName,
