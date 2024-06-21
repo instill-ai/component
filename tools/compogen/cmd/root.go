079b386af63ad81cbd8ab8ba6d4434d0525dd7b0
Juan Vallés <jvalles@instill.tech>
2024-06-21 19:22:34 +0200

    feat(compogen): bump version

--- tools/compogen/cmd/root.go
+++ tools/compogen/cmd/root.go
@@ -12,7 +12,7 @@ var rootCmd = &cobra.Command{
 	Use:     "compogen",
 	Short:   "compogen is the Instill AI component schema generation tool",
 	Long:    "compogen is the Instill AI component schema generation tool",
-	Version: "1.0.0",
+	Version: "1.1.0",
 	// We print errors ourselves rather than letting Cobra do it.
 	// This lets us print usage information selectively.
 	SilenceErrors: true,
