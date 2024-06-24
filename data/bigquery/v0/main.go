4d2e7ecc10904a93ffa32dd4600856dccbba68b7
ChunHao <64747455+chuang8511@users.noreply.github.com>
2024-06-24 03:58:24 +0100

    feat: add read task in bigquery component (#156)
    Because
    
    - we want to read from data components
    
    This commit
    
    - add read task to bigquery

--- data/bigquery/v0/main.go
+++ data/bigquery/v0/main.go
@@ -9,7 +9,10 @@ import (
 	"sync"
 
 	"cloud.google.com/go/bigquery"
+	pb "github.com/instill-ai/protogen-go/vdp/pipeline/v1beta"
+	"google.golang.org/api/iterator"
 	"google.golang.org/api/option"
+	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/structpb"
 
 	"github.com/instill-ai/component/base"
@@ -17,7 +20,10 @@ import (
 
 const (
 	taskInsert = "TASK_INSERT"
+	taskRead   = "TASK_READ"
 )
+
+var instillUpstreamTypes = []string{"value", "reference", "template"}
 
 //go:embed config/definition.json
 var definitionJSON []byte
@@ -102,6 +108,27 @@ func (e *execution) Execute(ctx context.Context, inputs []*structpb.Struct) ([]*
 				return nil, err
 			}
 			output = &structpb.Struct{Fields: map[string]*structpb.Value{"status": {Kind: &structpb.Value_StringValue{StringValue: "success"}}}}
+		case taskRead:
+
+			inputStruct := ReadInput{
+				ProjectID: getProjectID(e.Setup),
+				DatasetID: getDatasetID(e.Setup),
+				TableName: getTableName(e.Setup),
+				Client:    client,
+			}
+			err := base.ConvertFromStructpb(input, &inputStruct)
+			if err != nil {
+				return nil, err
+			}
+			outputStruct, err := readDataFromBigQuery(inputStruct)
+			if err != nil {
+				return nil, err
+			}
+			output, err = base.ConvertToStructpb(outputStruct)
+			if err != nil {
+				return nil, err
+			}
+
 		default:
 			return nil, fmt.Errorf("unsupported task: %s", e.Task)
 		}
@@ -121,4 +148,148 @@ func (c *component) Test(sysVars map[string]any, setup *structpb.Struct) error {
 		return nil
 	}
 	return errors.New("project ID does not match")
+}
+
+type TableColumns struct {
+	TableName string
+	Columns   []Column
+}
+
+type Column struct {
+	Name string
+	Type string
+}
+
+func (c *component) GetDefinition(sysVars map[string]any, compConfig *base.ComponentConfig) (*pb.ComponentDefinition, error) {
+
+	ctx := context.Background()
+	oriDef, err := c.Component.GetDefinition(nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if compConfig == nil {
+		return oriDef, nil
+	}
+
+	def := proto.Clone(oriDef).(*pb.ComponentDefinition)
+	client, err := NewClient(compConfig.Setup["json-key"].(string), compConfig.Setup["project-id"].(string))
+	if err != nil || client == nil {
+		return nil, fmt.Errorf("error creating BigQuery client: %v", err)
+	}
+	defer client.Close()
+
+	myDataset := client.Dataset(compConfig.Setup["dataset-id"].(string))
+	tables, err := constructTableColumns(myDataset, ctx, compConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	tableProperties, err := constructTableProperties(tables)
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO: chuang8511, remove table from definition.json and make it dynamic.
+	// It will be changed before 2024-06-26.
+	tableProperty := tableProperties[0]
+	for _, sch := range def.Spec.ComponentSpecification.Fields["oneOf"].GetListValue().Values {
+		data := sch.GetStructValue().Fields["properties"].GetStructValue().Fields["input"].GetStructValue().Fields["properties"].GetStructValue().Fields["data"].GetStructValue()
+		if data != nil {
+			data.Fields["properties"] = structpb.NewStructValue(tableProperty)
+		}
+	}
+
+	for _, dataSpec := range def.Spec.DataSpecifications {
+		dataInput := dataSpec.Input.Fields["properties"].GetStructValue().Fields["data"].GetStructValue()
+		if dataInput != nil {
+			dataInput.Fields["properties"] = structpb.NewStructValue(tableProperty)
+		}
+		dataOutput := dataSpec.Output.Fields["properties"].GetStructValue().Fields["data"].GetStructValue()
+
+		if dataOutput != nil {
+			aPieceData := dataOutput.Fields["items"].GetStructValue()
+			if aPieceData != nil {
+				aPieceData.Fields["properties"] = structpb.NewStructValue(tableProperty)
+			}
+
+		}
+	}
+
+	return def, nil
+}
+
+func constructTableColumns(myDataset *bigquery.Dataset, ctx context.Context, compConfig *base.ComponentConfig) ([]TableColumns, error) {
+	tableIT := myDataset.Tables(ctx)
+	tables := []TableColumns{}
+	for {
+		table, err := tableIT.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		tableName := table.TableID
+		tableDetail := myDataset.Table(tableName)
+		metadata, err := tableDetail.Metadata(ctx)
+		if err != nil {
+			return nil, err
+		}
+		schema := metadata.Schema
+		columns := []Column{}
+		for _, field := range schema {
+			columns = append(columns, Column{Name: field.Name, Type: string(field.Type)})
+		}
+
+		// TODO: chuang8511, remove table from definition.json and make it dynamic.
+		// It will be changed before 2024-06-26.
+		if compConfig.Setup["table-name"].(string) == tableName {
+			tables = append(tables, TableColumns{TableName: tableName, Columns: columns})
+		}
+	}
+	if len(tables) == 0 {
+		return nil, fmt.Errorf("table name is not found in the dataset")
+	}
+	return tables, nil
+}
+
+func constructTableProperties(tables []TableColumns) ([]*structpb.Struct, error) {
+	tableProperties := make([]*structpb.Struct, len(tables))
+
+	for idx, table := range tables {
+		propertiesMap := make(map[string]interface{})
+		for idx, column := range table.Columns {
+			propertiesMap[column.Name] = map[string]interface{}{
+				"title":                column.Name,
+				"instillUIOrder":       idx,
+				"description":          "Column " + column.Name + " of table " + table.TableName,
+				"instillFormat":        getInstillAcceptFormat(column.Type),
+				"instillUpstreamTypes": instillUpstreamTypes,
+				"instillAcceptFormats": []string{getInstillAcceptFormat(column.Type)},
+				"required":             []string{},
+				"type":                 getInstillAcceptFormat(column.Type),
+			}
+		}
+		propertyStructPB, err := base.ConvertToStructpb(propertiesMap)
+		if err != nil {
+			return nil, err
+		}
+
+		tableProperties[idx] = propertyStructPB
+	}
+	return tableProperties, nil
+}
+
+func getInstillAcceptFormat(tableType string) string {
+	switch tableType {
+	case "STRING":
+		return "string"
+	case "INTEGER":
+		return "integer"
+	case "BOOLEAN":
+		return "boolean"
+	default:
+		return "string"
+	}
 }
