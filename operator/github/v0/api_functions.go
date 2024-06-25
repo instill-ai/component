4cd864bb2f01634ccb6b5fb4523fcfa6a37d25b1
YCK1130 <ken1130yang@gmail.com>
2024-06-25 14:32:32 +0100

    feat: add get single pr func

--- operator/github/v0/api_functions.go
+++ operator/github/v0/api_functions.go
@@ -1,48 +1,29 @@
 package github
 
 import (
-	"context"
+	"fmt"
 
 	"github.com/google/go-github/v62/github"
-	"github.com/instill-ai/component/base"
+	"github.com/instill-ai/x/errmsg"
 	"google.golang.org/protobuf/types/known/structpb"
 )
 
 
-type GetPullRequestsResp struct {
-	PullRequests   []PullRequests `json:"pull-requests"`
-}
-type PullRequests struct {
-	Title	 	string 			`string:"title"`
-	Body 		string 			`string:"body"`
-}
 type GitHubClient struct {
 	client *github.Client
 	owner string
 	repository string
 }
 
-func (githubClient *GitHubClient) setTargetRepo(setup *structpb.Struct) {
+func (githubClient *GitHubClient) setTargetRepo(setup *structpb.Struct) error {
 	githubClient.owner = setup.GetFields()["owner"].GetStringValue()
 	githubClient.repository = setup.GetFields()["repository"].GetStringValue()
-}
-func (githubClient *GitHubClient) getPRs(props *structpb.Struct) (*structpb.Struct, error) {
-	githubClient.setTargetRepo(props)
-	prs, _, err := githubClient.client.PullRequests.List(context.Background(), githubClient.owner, githubClient.repository, nil)
-	if err != nil {
-		return nil, err
-	}
 
-	var prResp GetPullRequestsResp
-	prResp.PullRequests = make([]PullRequests, len(prs))
-	for idx, pr := range prs {
-		prResp.PullRequests[idx].Title = pr.GetTitle()
-		prResp.PullRequests[idx].Body = pr.GetBody()
+	if githubClient.owner == "" || githubClient.repository == "" {
+		return errmsg.AddMessage(
+			fmt.Errorf("owner or repository not provided"),
+			"owner or repository not provided",
+		)
 	}
-
-	out, err := base.ConvertToStructpb(prResp)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
+	return nil
 }
