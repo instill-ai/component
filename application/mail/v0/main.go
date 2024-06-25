63856489c81201cfa4efe831da17fa88aa12e4e5
chuang8511 <a0961192312@gmail.com>
2024-06-25 20:29:32 +0100

    feat: add read mail function

---
+++ application/mail/v0/main.go
@@ -0,0 +1,87 @@
+//go:generate compogen readme ./config ./README.mdx
+package mail
+
+import (
+	"context"
+	_ "embed"
+	"fmt"
+	"sync"
+
+	"github.com/instill-ai/component/base"
+	"github.com/instill-ai/x/errmsg"
+	"google.golang.org/protobuf/types/known/structpb"
+)
+
+const (
+	taskReadEmails = "TASK_READ_EMAILS"
+	taskSendEmails = "TASK_SEND_EMAILS"
+)
+
+var (
+	//go:embed config/definition.json
+	definitionJSON []byte
+	//go:embed config/setup.json
+	setupJSON []byte
+	//go:embed config/tasks.json
+	tasksJSON []byte
+
+	once sync.Once
+	comp *component
+)
+
+type component struct {
+	base.Component
+}
+
+type execution struct {
+	base.ComponentExecution
+
+	execute func(*structpb.Struct) (*structpb.Struct, error)
+}
+
+func Init(bc base.Component) *component {
+	once.Do(func() {
+		comp = &component{Component: bc}
+		err := comp.LoadDefinition(definitionJSON, setupJSON, tasksJSON, nil)
+		if err != nil {
+			fmt.Println("Failed to load definition", err)
+			panic(err)
+		}
+	})
+	return comp
+}
+
+func (c *component) CreateExecution(sysVars map[string]any, setup *structpb.Struct, task string) (*base.ExecutionWrapper, error) {
+	e := &execution{
+		ComponentExecution: base.ComponentExecution{Component: c, SystemVariables: sysVars, Setup: setup, Task: task},
+	}
+
+	switch task {
+	case taskReadEmails:
+		e.execute = e.readEmails
+	case taskSendEmails:
+		e.execute = e.sendEmails
+	default:
+		return nil, errmsg.AddMessage(
+			fmt.Errorf("not supported task: %s", task),
+			fmt.Sprintf("%s task is not supported.", task),
+		)
+	}
+
+	return &base.ExecutionWrapper{Execution: e}, nil
+}
+
+func (e *execution) Execute(_ context.Context, inputs []*structpb.Struct) ([]*structpb.Struct, error) {
+	outputs := make([]*structpb.Struct, len(inputs))
+
+	for i, input := range inputs {
+		output, err := e.execute(input)
+		if err != nil {
+			return nil, err
+		}
+
+		outputs[i] = output
+	}
+
+	return outputs, nil
+}
---
+++ application/mail/v0/read_emails.go
@@ -0,0 +1,246 @@
+package mail
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/emersion/go-imap/v2"
+	"github.com/emersion/go-imap/v2/imapclient"
+	"github.com/emersion/go-message/mail"
+	"github.com/instill-ai/component/base"
+	"google.golang.org/protobuf/types/known/structpb"
+)
+
+type ReadEmailsInput struct {
+	Search Search `json:"search"`
+}
+
+type Search struct {
+	ServerAddress     string `json:"server-address"`
+	ServerPort        int    `json:"server-port"`
+	Mailbox           string `json:"mailbox"`
+	SearchSubjectText string `json:"search-subject-text,omitempty"`
+	SearchFromEmail   string `json:"search-from-email,omitempty"`
+	SearchToEmail     string `json:"search-to-email,omitempty"`
+	Limit             int    `json:"limit,omitempty"`
+}
+
+type ReadEmailsOutput struct {
+	Emails []Email `json:"emails"`
+}
+
+type Email struct {
+	Date    string   `json:"date"`
+	From    string   `json:"from"`
+	To      []string `json:"to,omitempty"`
+	Subject string   `json:"subject"`
+	Message string   `json:"message,omitempty"`
+}
+
+func (e *execution) readEmails(input *structpb.Struct) (*structpb.Struct, error) {
+	inputStruct := ReadEmailsInput{}
+
+	err := base.ConvertFromStructpb(input, &inputStruct)
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := initIMAPClient(
+		inputStruct.Search.ServerAddress,
+		inputStruct.Search.ServerPort,
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer client.Close()
+
+	setup := e.GetSetup()
+	client.Login(
+		setup.GetFields()["email-address"].GetStringValue(),
+		setup.GetFields()["password"].GetStringValue(),
+	).Wait()
+
+	emails, err := fetchEmails(client, inputStruct.Search)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := client.Logout().Wait(); err != nil {
+		return nil, err
+	}
+
+	outputStruct := ReadEmailsOutput{
+		Emails: emails,
+	}
+
+	output, err := base.ConvertToStructpb(outputStruct)
+	if err != nil {
+		return nil, err
+	}
+
+	return output, nil
+}
+
+func initIMAPClient(serverAddress string, serverPort int) (*imapclient.Client, error) {
+
+	c, err := imapclient.DialTLS(serverAddress+":"+strconv.Itoa(serverPort), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
+func fetchEmails(c *imapclient.Client, search Search) ([]Email, error) {
+
+	mailbox := search.Mailbox
+	imapMailbox := convertMailboxString(mailbox)
+
+	selectedMbox, err := c.Select(imapMailbox, nil).Wait()
+	if err != nil {
+		return nil, err
+	}
+
+	if selectedMbox.NumMessages == 0 {
+		return []Email{}, nil
+	}
+
+	emails := []Email{}
+
+	if search.Limit == 0 {
+		// To avoid fetching all emails, we limit the number of emails to 100.
+		search.Limit = 100
+	}
+	limit := search.Limit
+
+	// TODO: chuang8511, make it concurrent.
+	// It will be done before 2024-07-26.
+	for i := selectedMbox.NumMessages; limit > 0; i-- {
+		limit--
+		email := Email{}
+		var seqSet imap.SeqSet
+		seqSet.AddNum(i)
+
+		fetchOptions := &imap.FetchOptions{
+			BodySection: []*imap.FetchItemBodySection{{}},
+		}
+		fetchCmd := c.Fetch(seqSet, fetchOptions)
+		msg := fetchCmd.Next()
+		var bodySection imapclient.FetchItemDataBodySection
+		ok := false
+		for {
+			item := msg.Next()
+			if item == nil {
+				break
+			}
+			bodySection, ok = item.(imapclient.FetchItemDataBodySection)
+			if ok {
+				break
+			}
+		}
+		if !ok {
+			return nil, fmt.Errorf("FETCH command did not return body section")
+		}
+
+		mr, err := mail.CreateReader(bodySection.Literal)
+		if err != nil {
+			return nil, fmt.Errorf("FETCH command did not return body section")
+		}
+
+		h := mr.Header
+		setEnvelope(&email, h)
+
+		if !includeMail(email, search) {
+			if err := fetchCmd.Close(); err != nil {
+				return nil, err
+			}
+			continue
+		}
+
+		for {
+			p, err := mr.NextPart()
+			if err == io.EOF {
+				break
+			}
+			inlineHeader, ok := p.Header.(*mail.InlineHeader)
+			if ok && !isHTMLType(inlineHeader) {
+				b, _ := io.ReadAll(p.Body)
+				email.Message += string(b)
+			}
+		}
+
+		if err := fetchCmd.Close(); err != nil {
+			return nil, err
+		}
+
+		emails = append(emails, email)
+	}
+
+	return emails, nil
+}
+
+func convertMailboxString(mailbox string) string {
+	switch {
+	case mailbox == "INBOX":
+		return "INBOX"
+	case mailbox == "SENT":
+		return "[Gmail]/Sent Mail"
+	case mailbox == "DRAFTS":
+		return "[Gmail]/Drafts"
+	default:
+		return "INBOX"
+	}
+}
+
+func setEnvelope(email *Email, h mail.Header) {
+	if date, err := h.Date(); err != nil {
+		log.Fatalf("failed to get date: %v", err)
+	} else {
+		email.Date = date.Format(time.DateTime)
+	}
+	if from, err := h.AddressList("From"); err != nil {
+		log.Fatalf("failed to get from: %v", err)
+	} else {
+		email.From = from[0].String()
+	}
+	if to, err := h.AddressList("To"); err != nil {
+		log.Fatalf("failed to get to: %v", err)
+	} else {
+		email.To = []string{}
+		for _, t := range to {
+			email.To = append(email.To, t.String())
+		}
+	}
+	if subject, err := h.Text("Subject"); err != nil {
+		log.Fatalf("failed to get subject: %v", err)
+	} else {
+		email.Subject = subject
+	}
+}
+
+func includeMail(email Email, search Search) bool {
+	if search.SearchSubjectText != "" {
+		if !strings.Contains(email.Subject, search.SearchSubjectText) {
+			return false
+		}
+	}
+	if search.SearchFromEmail != "" {
+		if !strings.Contains(email.From, search.SearchFromEmail) {
+			return false
+		}
+	}
+	if search.SearchToEmail != "" {
+		if !strings.Contains(strings.Join(email.To, ","), search.SearchToEmail) {
+			return false
+		}
+	}
+	return true
+}
+
+func isHTMLType(inlineHeader *mail.InlineHeader) bool {
+	return strings.Contains(inlineHeader.Get("Content-Type"), "text/html")
+}
