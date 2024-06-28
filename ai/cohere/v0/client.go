b001bc3ed86db63d0d3677363c338c1c08a9b91f
andre.liang <namwoam@gmail.com>
2024-06-28 11:39:53 +0100

    add: added citation fields

--- ai/cohere/v0/client.go
+++ ai/cohere/v0/client.go
@@ -3,6 +3,7 @@ package cohere
 import (
 	"context"
 	"fmt"
+	"sync"
 
 	cohereSDK "github.com/cohere-ai/cohere-go/v2"
 	cohereClientSDK "github.com/cohere-ai/cohere-go/v2/client"
@@ -13,26 +14,28 @@ import (
 type cohereClient struct {
 	sdkClient *cohereClientSDK.Client
 	logger    *zap.Logger
+	lock      sync.Mutex
 }
 
 func newClient(apiKey string, logger *zap.Logger) *cohereClient {
-	fmt.Printf("### API key: %s.\n", apiKey)
+	fmt.Printf("### API key: %s\n", apiKey)
 	client := cohereClientSDK.NewClient(cohereClientSDK.WithToken(apiKey))
-	return &cohereClient{sdkClient: client, logger: logger}
+	return &cohereClient{sdkClient: client, logger: logger, lock: sync.Mutex{}}
 }
 
-func (cl *cohereClient) generateTextChat(request cohereSDK.ChatRequest) (*cohereSDK.NonStreamedChatResponse, error) {
-	fmt.Println("### Generating text chat")
-
-	resp, err := cl.sdkClient.Chat(
+func (cl *cohereClient) generateTextChat(request cohereSDK.ChatRequest) (cohereSDK.NonStreamedChatResponse, error) {
+	respPtr, err := cl.sdkClient.Chat(
 		context.TODO(),
 		&request,
 	)
 	if err != nil {
-		fmt.Println("### An error has occurred")
-		return resp, err
+		panic(err)
 	}
-	fmt.Printf("### Text chat generated: %s\n", resp.Text)
+	resp := cohereSDK.NonStreamedChatResponse{
+		Text:         respPtr.Text,
+		GenerationId: respPtr.GenerationId,
+		Citations:    respPtr.Citations,
+	}
 	return resp, nil
 }
 
