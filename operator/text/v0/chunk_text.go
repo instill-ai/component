041f404d42a31fdb40147a9ea64a3304fb33b3b9
chuang8511 <a0961192312@gmail.com>
2024-07-16 13:17:47 +0100

    fix: bug of out the range of slice

--- operator/text/v0/chunk_text.go
+++ operator/text/v0/chunk_text.go
@@ -126,6 +126,12 @@ func chunkText(input ChunkTextInput) (ChunkTextOutput, error) {
 		chunkRunes := []rune(chunk)
 
 		startPosition, endPosition := positionCalculator.getChunkPositions(rawRunes, chunkRunes, startScanPosition)
+
+
+		if shouldScanRawTextFromBeginning(startPosition, endPosition) {
+			startPosition, endPosition = positionCalculator.getChunkPositions(rawRunes, chunkRunes, 0)
+		}
+
 		output.TextChunks = append(output.TextChunks, TextChunk{
 			Text:          chunk,
 			StartPosition: startPosition,
@@ -135,6 +141,10 @@ func chunkText(input ChunkTextInput) (ChunkTextOutput, error) {
 	}
 
 	return output, nil
+}
+
+func shouldScanRawTextFromBeginning(startPosition, endPosition int) bool {
+	return startPosition == 0 && endPosition == 0
 }
 
 type ChunkPositionCalculator interface {
@@ -147,6 +157,11 @@ func (PositionCalculator) getChunkPositions(rawText, chunk []rune, startScanPosi
 
 	for i := startScanPosition; i < len(rawText); i++ {
 		if rawText[i] == chunk[0] {
+
+			if i+len(chunk) > len(rawText) {
+				break
+			}
+
 			if reflect.DeepEqual(rawText[i:i+len(chunk)], chunk) {
 				startPosition = i
 				endPosition = len(chunk) + i - 1
@@ -166,6 +181,11 @@ func (MarkdownPositionCalculator) getChunkPositions(rawText, chunk []rune, start
 	for i := startScanPosition; i < len(rawText); i++ {
 
 		if rawText[i] == chunk[skipHeaderIndex] {
+
+			if i+len(chunk)-skipHeaderIndex > len(rawText) {
+				break
+			}
+
 			if reflect.DeepEqual(rawText[i:(i+len(chunk)-skipHeaderIndex)], chunk[skipHeaderIndex:]) {
 				startPosition = i
 				endPosition = len(chunk) + i - 1 - skipHeaderIndex
