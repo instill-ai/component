52e201ba03809f8dc4c26a3ce6c13e212d8b25c2
Yazidane <zidanehakimgt@gmail.com>
2024-08-23 22:43:01 +0800

    feat: add zilliz component

---
+++ data/zilliz/v0/client.go
@@ -0,0 +1,26 @@
+package zilliz
+
+import (
+	"github.com/instill-ai/component/internal/util/httpclient"
+	"go.uber.org/zap"
+	"google.golang.org/protobuf/types/known/structpb"
+)
+
+func newClient(setup *structpb.Struct, logger *zap.Logger) *httpclient.Client {
+	c := httpclient.New("Zilliz", getURL(setup),
+		httpclient.WithLogger(logger),
+	)
+
+	c.SetHeader("Authorization", "Bearer "+getAPIKey(setup))
+	c.SetHeader("Content-Type", "application/json")
+
+	return c
+}
+
+func getURL(setup *structpb.Struct) string {
+	return setup.GetFields()["url"].GetStringValue()
+}
+
+func getAPIKey(setup *structpb.Struct) string {
+	return setup.GetFields()["api-key"].GetStringValue()
+}
