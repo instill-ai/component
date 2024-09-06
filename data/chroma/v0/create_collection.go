9fa57f68a7ffd3459982781a222f89b40916c6ea
Zoodane <138553907+zidanehakim@users.noreply.github.com>
2024-09-06 16:26:17 +0800

    feat: add chroma component (#300)
    Because
    
    - We want to integrate VDP with chroma self hosted vector db
    - We want more vector db component choice
    
    This commit
    
    - Add chroma component along with the basic tasks (query, upsert, ..)
    - Use api key auth that user determine themselves

---
+++ data/chroma/v0/create_collection.go
@@ -0,0 +1,82 @@
+package chroma
+
+import (
+	"fmt"
+
+	"github.com/instill-ai/component/base"
+	"google.golang.org/protobuf/types/known/structpb"
+)
+
+const (
+	createCollectionPath = "/api/v1/collections"
+)
+
+type CreateCollectionOutput struct {
+	Status string `json:"status"`
+}
+
+type CreateCollectionInput struct {
+	CollectionName string         `json:"collection-name"`
+	Configuration  map[string]any `json:"configuration"`
+	Metadata       map[string]any `json:"metadata"`
+	GetOrCreate    bool           `json:"get-or-create"`
+}
+
+type CreateCollectionReq struct {
+	Name          string         `json:"name"`
+	Configuration map[string]any `json:"configuration"`
+	Metadata      map[string]any `json:"metadata"`
+	GetOrCreate   bool           `json:"get_or_create"`
+}
+
+type CreateCollectionResp struct {
+	Detail []map[string]any `json:"detail"`
+}
+
+func (e *execution) createCollection(in *structpb.Struct) (*structpb.Struct, error) {
+	var inputStruct CreateCollectionInput
+	err := base.ConvertFromStructpb(in, &inputStruct)
+	if err != nil {
+		return nil, err
+	}
+
+	resp := CreateCollectionResp{}
+
+	reqParams := CreateCollectionReq{
+		Name:        inputStruct.CollectionName,
+		GetOrCreate: inputStruct.GetOrCreate,
+	}
+	if inputStruct.Metadata != nil {
+		reqParams.Metadata = inputStruct.Metadata
+	}
+	if inputStruct.Configuration != nil {
+		reqParams.Configuration = inputStruct.Configuration
+	}
+
+	req := e.client.R().SetBody(reqParams).SetResult(&resp)
+
+	res, err := req.Post(createCollectionPath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode() != 200 {
+		return nil, fmt.Errorf("failed to create collection: %s", res.String())
+	}
+
+	if resp.Detail != nil {
+		return nil, fmt.Errorf("failed to create collection: %s", resp.Detail[0]["msg"])
+	}
+
+	outputStruct := CreateCollectionOutput{
+		Status: "Successfully created 1 collection",
+	}
+
+	output, err := base.ConvertToStructpb(outputStruct)
+	if err != nil {
+		return nil, err
+	}
+
+	return output, nil
+}
