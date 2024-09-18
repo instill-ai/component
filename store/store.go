b12d33559e2d7de7d058509f24bfbe632741a19e
chuang8511 <a0961192312@gmail.com>
2024-09-18 15:42:50 +0100

    chore: take out unfinished component first

--- store/store.go
+++ store/store.go
@@ -19,7 +19,6 @@ import (
 	"github.com/instill-ai/component/ai/ollama/v0"
 	"github.com/instill-ai/component/ai/openai/v0"
 	"github.com/instill-ai/component/ai/stabilityai/v0"
-	"github.com/instill-ai/component/ai/universalai/v0"
 	"github.com/instill-ai/component/application/asana/v0"
 	"github.com/instill-ai/component/application/email/v0"
 	"github.com/instill-ai/component/application/freshdesk/v0"
@@ -117,12 +116,6 @@ func Init(
 		compStore.Import(instill.Init(baseComp))
 		compStore.Import(huggingface.Init(baseComp))
 
-		{
-			// UniversalAI
-			conn := universalai.Init(baseComp)
-			conn = conn.WithInstillCredentials(secrets[conn.GetDefinitionID()])
-			compStore.Import(conn)
-		}
 		{
 			// OpenAI
 			conn := openai.Init(baseComp)
